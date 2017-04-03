5175e5402d138446519523eec428d10ee21fceaf
ivan_surus <ivan_surus@epam.com>
2017-04-03 15:46:31 +0300

    initial commit

---
+++ src/main/java/builder/TestBuilderPattern.java
@@ -0,0 +1,15 @@
+package builder;
+
+public class TestBuilderPattern {
+
+    public static void main(String[] args) {
+
+        //Using builder to get the object in a single line of code and
+         //without any inconsistent state or arguments management issues
+
+        Computer comp = new Computer.ComputerBuilder("500 GB", "2 GB").setBluetoothEnabled(true).setGraphicsCardEnabled(true).build();
+        System.out.println(comp.toString());
+        Computer comp2 = new Computer.ComputerBuilder("0", "0").build();
+        System.out.println(comp2.toString());
+    }
+}
